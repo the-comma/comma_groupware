ad550ee47bcc98267ce868b70ae0a3f2385b9e24
ryubin0509 <bin00509@naver.com>
2025-08-14 16:32:08 +0900

    [유빈] Dto 분할작업 누락내용

--- src/main/java/com/example/comma_groupware/dto/EquipmentRequest.java
+++ src/main/java/com/example/comma_groupware/dto/EquipmentRequest.java
@@ -1,5 +1,17 @@
 package com.example.comma_groupware.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
 public class EquipmentRequest {
 
+	private int requestId;
+	private int projectId;
+	private int empId;
+	private int equipmentId;
+	private int quantity;
+	private LocalDateTime requestDate;
+	private String requestStatus;
 }
--- src/main/java/com/example/comma_groupware/dto/EquipmentUsage.java
+++ src/main/java/com/example/comma_groupware/dto/EquipmentUsage.java
@@ -1,5 +1,17 @@
 package com.example.comma_groupware.dto;
 
-public class EquipmentUsage {
+import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
+public class EquipmentUsage {
+	private int usageId;
+	private int projectId;
+	private int empId;
+	private int equipmentId;
+	private int usageQuantity;
+	private LocalDateTime issuedDate;
+	private String usageStatus;
+	private int damagedReported;
 }
